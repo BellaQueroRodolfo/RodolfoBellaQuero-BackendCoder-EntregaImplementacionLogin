416a0a04d7340847f92bd1fa67084abf239a790b
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-11-28 19:27:45 -0300

    Update app.js

--- BellaQueroRodolfo-BackendCoder-DesafioImplementacionLogin/src/app.js
+++ BellaQueroRodolfo-BackendCoder-DesafioImplementacionLogin/src/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/User');
 const app = express();
 const port = 8080;
 
@@ -14,7 +18,20 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'src', 'views'));
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+}, User.authenticate()));
 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 mongoose.connect('mongodb+srv://<username>:<password>@cluster.mongodb.net/ecommerce', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -25,16 +42,16 @@ const db = mongoose.connection;
 db.on('error', (error) => {
   console.error('MongoDB connection error:', error);
 });
-
 db.once('open', () => {
   console.log('Connected to MongoDB Atlas');
 });
 
 const productsRouter = require('./src/routes/products');
 const cartsRouter = require('./src/routes/carts');
-
+const authRouter = require('./src/routes/auth');
 app.use('/products', productsRouter);
 app.use('/carts', cartsRouter);
+app.use('/auth', authRouter);
 app.get('/chat', (req, res) => {
   res.render('chat', { layout: 'main' });
 });
